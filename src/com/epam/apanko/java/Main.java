6b725ed53b97b148ae43eab672db0963f89acae4
Artsiom <artsiom_panko@epam.com>
2019-07-29 14:32:46 +0300

    Again and again

--- src/com/epam/apanko/java/Main.java
+++ src/com/epam/apanko/java/Main.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        Map<String, String> f;
+        Map<String, String> map;
+        System.out.println();
+        System.out.println();
     }
 }
